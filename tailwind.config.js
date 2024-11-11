73cbc5f8c5bc465b2a2326f8837d362c9aab96b9
Jeffrey Patey <jeffreyvpatey@gmail.com>
2024-11-11 14:00:16 -0400

    installed tailwind css for react

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        clean: "#D9D9D9",
+        paper: "#E9E9E9",
+        feather: "#77C9D4",
+        feather_2: "#27bfd3",
+        marine: "#57BC90",
+        forest: "#015249",
+        grey: "#9099A2",
+        charcoal: "#3C3C3C",
+        steel: "#31363F",
+        smoke: "#222831d9",
+        slate: "#222831",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
