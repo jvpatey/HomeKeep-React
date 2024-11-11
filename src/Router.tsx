c494c1f22fb028bdbc0d73c7a46e853dd1108d85
Jeffrey Patey <jeffreyvpatey@gmail.com>
2024-11-11 14:59:25 -0400

    removed unnecessary login component

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,11 @@
 // src/routes/AppRouter.tsx
 import { Routes, Route } from "react-router-dom";
-import { Login } from "./components/Login";
 import { Home } from "./components/Home";
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/signin" element={<Login />} />
     </Routes>
   );
 }
